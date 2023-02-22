2e81b4831a6db7ace5fd037963e372917254bc56
rivabayu <rivabays@gmail.com>
2023-02-22 16:30:48 +0700

    skeleton loading

--- src/components/product-detail/layouts/Description.jsx
+++ src/components/product-detail/layouts/Description.jsx
@@ -1,45 +1,66 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Skeleton from '@mui/material/Skeleton'
+import Box from '@mui/material/Box';
 
 function Description({ data }) {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() =>{
+    setTimeout(()=> {
+      setLoading(true)
+    }, 2000)
+  })
   return (
     <section>
-      <div className="mb-3">
-        <h2 className="font-bold text-xl">{data.title}</h2>
-      </div>
-
-      <div className="flex items-center space-x-4 my-3">
-        <p className="font-normal text-sm">Terjual 10RB+</p>
-        <p className="font-normal text-xs">⭐ {data && data.rating}</p>
-      </div>
-
-      <div className="my-1">
-        <h3 className="font-bold text-3xl">${data && data.price}</h3>
-      </div>
-
-      <div className="flex items-center space-x-2 my-1 mb-7">
-        <div className="rounded-lg bg-red-300 px-1 py-0">
-          <p className="text-semibold font-normal text-red-600">
-            {data && Math.ceil(data.discountPercentage)}%
-          </p>
+      {loading ?(
+        <div className="">
+          <h2 className="font-bold text-xl">{data.title}</h2>
         </div>
-        <div>
-          <p className="font-normal text-sm line-through text-gray-500">
-            $
-            {data &&
-              Math.ceil((100 / (100 - data.discountPercentage)) * data.price)}
-          </p>
+      ): <Skeleton variant="rounded" animation='wave' width={150} sx={{marginBottom: 1.5 }}/>}
+      {loading ? (
+        <div className="flex items-center mt-2 space-x-4 my-3">
+          <p className="font-normal text-sm">Terjual 10RB+</p>
+          <p className="font-normal text-xs">⭐ {data && data.rating}</p>
         </div>
-      </div>
-      <div className="mb-6">
-        <p className="font-normal text-sm break-words">
-          {data && data.description}
-          {data && data.description.length > 300 && (
-            <span className="text-emerald-500 font-semibold hover:cursor-pointer">
-              Lihat Selengkapnya
-            </span>
-          )}
-        </p>
-      </div>
+      ): <Skeleton variant="rounded" animation='wave' width={150}     height={20} sx={{marginBottom: 1.5 }}/>}     
+      {loading ? (
+        <div className="my-1">
+          <h3 className="font-bold text-3xl">${data && data.price}</h3>
+        </div>
+      ): <Skeleton variant="rounded" animation='wave' width={100}     height={30} sx={{marginBottom: 1.5 }} />}     
+      {loading ? (  
+        <div className="flex items-center space-x-2 my-1 mb-7">
+          <div className="rounded-lg bg-red-300 px-1 py-0">
+            <p className="text-semibold font-normal text-red-600">
+              {data && Math.ceil(data.discountPercentage)}%
+            </p>
+          </div>
+          <div>
+            <p className="font-normal text-sm line-through text-gray-500">
+              $
+              {data &&
+                Math.ceil((100 / (100 - data.discountPercentage)) * data.price)}
+            </p>
+          </div>
+        </div>
+      ): <Skeleton variant="rounded" animation='wave' width={100}     height={25} sx={{marginBottom: 2.5 }}/>}     
+      {loading ? (  
+        <div className="mb-6">
+          <p className="font-normal text-sm break-words">
+            {data && data.description}
+            {data && data.description.length > 300 && (
+              <span className="text-emerald-500 font-semibold hover:cursor-pointer">
+                Lihat Selengkapnya
+              </span>
+            )}
+          </p>
+        </div>  
+      ):<Box sx={{ pt: 0.5 }}>
+          <Skeleton variant="rounded" animation='wave' sx={{marginBottom: 1 }} />
+          <Skeleton variant="rounded" animation='wave' sx={{marginBottom: 1 }} />
+          <Skeleton variant="rounded" animation='wave' sx={{marginBottom: 1 }} />  
+      </Box>}     
+
     </section>
   );
 }
--- src/components/product-detail/layouts/Toko.jsx
+++ src/components/product-detail/layouts/Toko.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Skeleton from '@mui/material/Skeleton'
 
 import toko1 from "../../../assets/images/img/toko.jpg";
 
 function Toko() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() =>{
+    setTimeout(()=> {
+      setLoading(true)
+    }, 2000)
+  })
   return (
     <div className="flex mt-4 hover:cursor-pointer w-52">
-      <div className="space-x-4 mr-4">
-        <img
-          className="rounded-full"
-          src={toko1}
-          alt="toko"
-          width="50px"
-          height="50px"
-        />
-      </div>
-      <div className="mb-6">
-        <h3 className="font-semibold text-base">Toko Sederhana</h3>
-        <p className="font-normal text-sm">Surabaya</p>
-      </div>
+       {loading ? (
+        <div className="space-x-4 mr-4">
+          <img
+            className="rounded-full"
+            src={toko1}
+            alt="toko"
+            width="50px"
+            height="50px"
+          />
+        </div>
+      ): <Skeleton animation="wave" variant="circular" width={50} height={50} sx={{marginRight : 2}} />} 
+      {loading ? (
+        <div className="mb-6">
+          <h3 className="font-semibold text-base">Toko Sederhana</h3>
+          <p className="font-normal text-sm">Surabaya</p>
+        </div>
+      ) : <Skeleton animation="wave" variant="rounded" width={150} height={50} />}
     </div>
   );
 }
