9ab9d0b883cba323107a9fb5a4c1dfe70fc00fa3
ronitaradipta <taradipta.roni@gmail.com>
2023-03-04 09:53:39 +0800

    fix login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
-import api from "../services/api";
+import callApi from "../services/callApi";
 import React, { useState } from "react";
-import Cookies from "js-cookie";
 import { useNavigate, Link } from "react-router-dom";
 import LeftContainer from "../components/auth/LeftContainer";
 import FormCard from "../components/auth/elements/FormCard";
@@ -8,11 +7,13 @@ import InputElement from "../components/auth/elements/InputElement";
 
 const LoginPage = () => {
   const [input, setInput] = useState({
-    username: "",
+    email: "",
     password: "",
   });
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const [Notification, setNotification] = useState(false);
+  const [SuccessMessage, setSuccessMessage] = useState("");
 
   const navigate = useNavigate();
 
@@ -20,25 +21,22 @@ const LoginPage = () => {
     try {
       e.preventDefault();
       setLoading(true);
-      const response = await api.post("/auth/login", {
-        username: input.username,
+      const response = await callApi.post("/auth/login", {
+        email: input.email,
         password: input.password,
       });
+      setSuccessMessage(response.data.message);
       setLoading(false);
-      navigate("/");
-      const token = response.data.token;
-      Cookies.set("token", token, { expires: 1 });
-      Cookies.set("user", JSON.stringify(response.data), { expires: 1 });
+      setNotification(true);
+      setTimeout(() => {
+        navigate("/otp-page");
+      }, 2500);
     } catch (error) {
       console.log(error);
-      setErrorMsg(error.response.data.message);
+      setErrorMsg(error);
       setLoading(false);
     }
   };
-
-  // USER LOGIN
-  // username: 'kminchelle',
-  //  password: '0lelplR',
 
   const handleChangeInput = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -48,9 +46,49 @@ const LoginPage = () => {
     <div className="flex h-screen flex-wrap">
       <LeftContainer />
       <div className=" w-full md:w-1/2 flex justify-center items-center bg-emerald-500">
-        <FormCard title="Login" button="LOG IN" link="Daftar" question="Pengguna Baru?" route="/register" onChange={handleChangeInput} loading={loading} onSubmit={loginUser} errMessage={errorMsg}>
-          <InputElement type="text" placeholder="Username/email" name="username" onChange={handleChangeInput} />
-          <InputElement type="password" placeholder="password" name="password" onChange={handleChangeInput} />
+        <div
+          className={`${
+            Notification ? "flex" : "hidden"
+          } mr-5 ml-5 fixed top-0 py-2 px-5 bg-green-500 opacity-0 rounded-md text-white translate-y-[150px] animate-popUp`}
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 mr-1.5 text-black flex-shrink-0"
+            fill="white"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <p>{SuccessMessage}</p>
+        </div>
+        <FormCard
+          title="Login"
+          button="LOG IN"
+          link="Daftar"
+          question="Pengguna Baru?"
+          route="/register"
+          onChange={handleChangeInput}
+          loading={loading}
+          onSubmit={loginUser}
+          errMessage={errorMsg}
+        >
+          <InputElement
+            type="text"
+            placeholder="Username/email"
+            name="email"
+            onChange={handleChangeInput}
+          />
+          <InputElement
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={handleChangeInput}
+          />
           <Link to="/forgot-password" className="text-sm text-gray-500">
             Lupa password ?
           </Link>
