303a6a7b8f0ae3c2143cfa7e2a05d7390d442be0
rivabayu <rivabays@gmail.com>
2023-03-09 15:27:11 +0700

    progres1

--- src/components/storepage/StoreProducts.jsx
+++ src/components/storepage/StoreProducts.jsx
@@ -2,22 +2,39 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SelectElement from "../SearchResults/SelectElement";
 import CardProductStore from "../homepage/element/CardProductStore";
-import api from "../../services/api";
+// import api from "../../services/api";
+import callApi from "../../services/callApi";
 
 const StoreProducts = () => {
   const { query, cat } = useParams();
   const [categories, setCategories] = useState([]);
   const [Products, setProducts] = useState([]);
+  const {idData} = useParams();
 
   const fetchAllCategory = async () => {
     try {
-      const category = await api.get("/products/categories");
-      setCategories(category.data);
+      // const category = await api.get("/products/categories");
+      const category = await callApi.get("/categories");
+      // setCategories(category.data); 
+      setCategories(category.data.data); 
+      console.log(category.data.data)
     } catch (error) {
       console.log(error);
     }
   };
 
+  // const fetchSearchProducts = async () => {
+  //   let param;
+  //   try {
+  //     if (query) {
+  //       param = `search/?q=${query}`;
+  //     }
+  //     if (cat) {
+  //       param = `category/${cat}`;
+  //     }
+  //     const response = await api.get(`/products`);
+  //     setProducts(response.data.products);
+  //   } catch (error) {}
   const fetchSearchProducts = async () => {
     let param;
     try {
@@ -27,8 +44,9 @@ const StoreProducts = () => {
       if (cat) {
         param = `category/${cat}`;
       }
-      const response = await api.get(`/products`);
-      setProducts(response.data.products);
+      const response = await callApi.get(`/stores/${idData}/products`);
+      setProducts(response.data.data);
+      console.log(response.data.data)
     } catch (error) {}
   };
 
@@ -45,19 +63,19 @@ const StoreProducts = () => {
       <div className="w-full md:w-1/5 mb-5">
         <div className="bg-white shadow-md rounded-lg p-4 mt-4">
           <div>
-            <h3 className="font-semibold mb-3">Etalase Toko</h3>
+            {/* <h3 className="font-semibold mb-3">Etalase Toko</h3>
             <SelectElement data={categories} />
           </div>
           <div>
             <h3 className="font-semibold mb-3">Kategori</h3>
-            <SelectElement data={categories} />
+            <SelectElement data={categories} /> */}
           </div>
         </div>
       </div>
       <div className="w-full md:w-[80%] pl-0 md:pl-5">
         <div className="flex flex-wrap ">
           {Products.length > 0 &&
-            Products.map((data) => {
+            Products.map((data, idx) => {
               return <CardProductStore data={data} key={data.id} />;
             })}
         </div>
