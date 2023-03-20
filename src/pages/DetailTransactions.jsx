6cdb0c5cd6e6bf9bd20518a616d249fd4cb35126
rivabayu <rivabays@gmail.com>
2023-03-20 13:09:32 +0700

    add orderlist

--- src/pages/DetailTransactions.jsx
+++ src/pages/DetailTransactions.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import TitlePage from "../components/DashboardStore/element/TitlePage";
 import LayoutDashboard from "../components/layout/LayoutDashboard";
-import api from "../services/api";
+import api from "../services/callApi";
 import productimg from "../assets/images/laptop-stand.png";
 
 const DetailTransactions = () => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [dataTransaction, setDataTransaction] = useState([]);
+  const [dataDetail, setDataDetail] = useState([]);
 
   const fetchDataTransaction = async () => {
     try {
-      const response = await api.get("/carts/?limit=5");
-      console.log(response.data.carts);
-      setDataTransaction(response.data.carts);
+      const response = await api.get("orders/store");
+      // console.log(response.data.data);
+      setDataTransaction(response.data.data);
+      
+      console.log(response.data.data.OrderDetails)
     } catch (error) {
       console.log(error);
     }
@@ -31,6 +34,7 @@ const DetailTransactions = () => {
   ];
 
   return (
+    // <></>
     <LayoutDashboard>
       <TitlePage title="Daftar Pesanan" />
       <div className="bg-white w-full pt-5 pl-5 pr-5 rounded-lg mt-6 flex gap-4 md:gap-8 lg:gap-12 items-center font-medium text-sm md:text-base text-gray-500">
@@ -52,19 +56,21 @@ const DetailTransactions = () => {
       {activeTabIndex === 0 && (
         <div className="flex flex-col gap-6 mt-6">
           {dataTransaction.length > 0 &&
-            dataTransaction.map((item) => {
-              const { title, discountedPrice, quantity } = item.products[0];
+            dataTransaction.map((data, ) => {
+              // console.log(dataTransaction)
+
+              const { customerAddress, customerDetail, totalPrice, orderDetails, orderStatus, shippingStatus, shippingCost, trackingNumber  } = data;
               return (
                 <div
                   className="bg-white w-full flex rounded-lg p-4 md:p-6 shadow-lg justify-between gap-2 flex-col lg:flex-row"
-                  key={item.id}
+                  key={data}
                 >
-                  <div className="flex gap-x-5 md:w-8/12 lg:w-6/12 xl:w-4/12">
-                    <img src={productimg} alt="image" className="w-16 h-16 md:w-24 md:h-24 " />
+                  {/* <div className="flex gap-x-5 md:w-8/12 lg:w-6/12 xl:w-4/12">
+                    <img src={orderDetails?.productId} alt="image" className="w-16 h-16 md:w-24 md:h-24 " />
                     <div className="flex flex-col justify-between">
-                      <div className="text-sm font-semibold md:text-[18px]">{title}</div>
+                      <div className="text-sm font-semibold md:text-[18px]">{ProductId}</div>
                       <p className="text-gray-500 text-sm md:text-base">
-                        {quantity} x {discountedPrice}
+                        {orderDetails.quantity} x {discountedPrice}
                       </p>
                       <a
                         href="#"
@@ -73,25 +79,25 @@ const DetailTransactions = () => {
                         Lihat Produk Lainnya
                       </a>
                     </div>
-                  </div>
+                  </div> */}
                   <div className=" md:w-8/12 lg:w-4/12 py-2">
                     <div className="font-semibold">Alamat</div>
                     <p className="text-gray-500 text-[10px] md:text-sm">
-                      Setra Duta Cemara, Kec. Kuta Selatan, Kota Badung, Provinsi Bali - 80361
+                      {customerAddress}
                     </p>
                   </div>
                   <div className="md:w-8/12 lg:w-4/12 py-2">
                     <div className="text-sm md:text-xl font-semibold">Nama Pemesan</div>
                     <p className="text-gray-500 text-[10px] md:text-sm">
-                      Asep Surasep (+628 2020 11111)
+                      {customerDetail}
                     </p>
                   </div>
                   <div className="md:w-8/12 lg:w-4/12 py-2">
                     <h3 className="text-gray-500 text-sm md:text-md">
-                      Jumlah Pesanan {item.totalProducts} Barang
+                      Jumlah Pesanan {orderDetails} Barang
                     </h3>
                     <p className="text-sm md:text-xl font-semibold">
-                      Total Harga : Rp.500.000
+                      Total Harga : Rp. {totalPrice}
                     </p>
                   </div>
                 </div>
