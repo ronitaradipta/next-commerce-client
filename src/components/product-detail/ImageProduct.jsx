606d8677df1ef86725b8ceeb0f41bf1d9d7a68f5
rivabayu <rivabays@gmail.com>
2023-03-22 20:36:14 +0700

    fixing image storePage

--- src/components/product-detail/ImageProduct.jsx
+++ src/components/product-detail/ImageProduct.jsx
@@ -15,7 +15,7 @@ function ImageDetail({ data }) {
       <section className="p-2 w-full sm:w-1/2 md:w-[33%]  mb-5 ">
         <div className="main-image">
           {loadingImg ? <div>
-            <button className="show-modal hover:cursor-pointer">{data && <img src={data.images[0].image} alt="" />}</button>
+            <button className="show-modal hover:cursor-pointer">{data && <img src={data.images[0]?.image} alt="" />}</button>
             </div> : <Skeleton variant="rounded" animation="wave" width="w-1/3" height={200} />}
         </div>
         <div className="flex items-center">
