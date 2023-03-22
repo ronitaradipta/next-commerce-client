45ae7f3a87f05500e8ba56ba93ea4b51f93dcafc
ronitaradipta <taradipta.roni@gmail.com>
2023-03-22 16:05:27 +0800

    fix bug header dashboard, and card product revision

--- src/components/homepage/element/CardProduct.jsx
+++ src/components/homepage/element/CardProduct.jsx
@@ -39,7 +39,7 @@ const CardProduct = ({ data }) => {
         <div className="p-3 flex flex-col gap-2">
           {loadingText ? (
             <div>
-              <h3 className="text-xs">{data.title}</h3>
+              <h3 className="text-base">{data.name}</h3>
             </div>
           ) : (
             <Skeleton animation="wave" variant="rounded" />
@@ -60,19 +60,23 @@ const CardProduct = ({ data }) => {
           )}
           <div className="flex justify-between items-center">
             <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6 text-yellow-400"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <p className="text-xs text-gray-400">{data.rating}</p>
+              {data.rating ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-yellow-400"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : null}
+              <p className="text-xs text-gray-400">
+                {data.rating || "tidak ada penilaian"}
+              </p>
             </div>
             <p className="text-xs text-gray-400">Terjual 3rb+</p>
           </div>
