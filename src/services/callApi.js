a7e9713e3396075ecd97f891fbb1dd2f88b5f634
Fiqri Oemry <121015122+FiqriReomy@users.noreply.github.com>
2023-03-23 15:11:16 +0700

    typo

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -1,9 +1,18 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: "http://localhost:8080",
   headers: { "content-type": "multipart/form-data" },
   withCredentials: true,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default instance;
