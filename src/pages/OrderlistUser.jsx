5b7ca77d33fe629d5b6bd93459700a4e608f277d
ronitaradipta <taradipta.roni@gmail.com>
2023-03-25 00:55:07 +0800

    add payment confirmation

--- src/pages/OrderlistUser.jsx
+++ src/pages/OrderlistUser.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
 import TitlePage from "../components/DashboardStore/element/TitlePage";
 import DetailTransakionList from "../components/DashboardUser/DetailTransakionList";
+import PaymentConfirmation from "../components/DashboardUser/UserElements/PaymentConfirmation";
 import Header from "../components/homepage/Header";
 import api from "../services/callApi";
 
 const OrderlistUser = () => {
   const [dataTransaction, setDataTransaction] = useState([]);
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   const fetchDataTransaction = async () => {
     try {
       const response = await api.get("orders/user");
       setDataTransaction(response.data.data);
-      console.log(response.data.data);
     } catch (error) {
       console.log(error);
     }
@@ -46,8 +55,17 @@ const OrderlistUser = () => {
   return (
     <div className="min-h-[100vh] bg-gray-200">
       <Header />
+      <PaymentConfirmation closeModal={closeModal} isOpen={isOpen} />
       <section className="p-10">
-        <TitlePage title="Riwayat Pesanan" />
+        <div className="flex justify-between">
+          <TitlePage title="Riwayat Pesanan" />
+          <button
+            className="text-white font-semibold bg-emerald-500 rounded-md py-2 px-4"
+            onClick={openModal}
+          >
+            Konfirmasi Pembayaran
+          </button>
+        </div>
         <div className="bg-white w-full pt-5 pl-5 pr-5 rounded-lg mt-6 flex gap-4 md:gap-8 lg:gap-12 items-center font-medium text-sm md:text-base text-gray-500">
           {menu.map((item, idx) => {
             return (
